ef200813d5e1aa13bd4d6db82440eff11b841c43
Shp1ker <romaboltuzkiy@gmail.com>
2022-09-30 19:33:36 +0300

    InputHandler

---
+++ FlappyBird/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private BirdMover _birdMover;
+    private InputScheme _inputs;
+    private void Awake()
+    {
+        if (_birdMover == null)
+        {
+            enabled = false;
+            return;
+        }
+    _inputs = new InputScheme();
+    }
+    private void OnEnable()
+    {
+        _inputs.Enable();
+        _inputs.ActionMap1.Touch.performed += OnTouch;
+    }
+    private void OnDisable()
+    {
+        _inputs.Disable();
+        _inputs.ActionMap1.Touch.performed += OnTouch;
+    }
+    private void OnTouch(CallbackContext context)
+    {
+        _birdMover.Jump();
+    }
+}
